2c60ba273faef9e356cac6780466ffb23d7bd374
XavierXinchi <Xinchi.Shi20@gmail.com>
2022-07-16 01:38:39 +0800

    Upload the 11th week's labs

---
+++ lab11/ARSet/ARSet.java
@@ -0,0 +1,110 @@
+package lab11.ARSet;
+
+import java.util.Iterator;
+
+public class ARSet<T> implements Iterable<T> {
+    private T[] items;
+    private int size;
+
+    /**
+     * Create an empty set.
+     */
+    @SuppressWarnings("unchecked")
+    public ARSet() {
+        items = (T[]) new Object[100];
+        size = 0;
+    }
+
+    /**
+     * @return the number of items in the set
+     */
+    public int size() {
+        return size;
+    }
+
+    /*
+     ***************************
+     * DO NOT MODIFY CODE ABOVE
+     ***************************
+     */
+
+
+    // Copy paste your code from Week 10 here
+
+    public Iterator<T> iterator() {
+
+        return new ARSetIterator();
+
+    }
+
+    private class ARSetIterator implements Iterator<T> {
+
+        private int index;
+
+        public ARSetIterator() {
+            this.index = 0;
+        }
+
+
+        @Override
+        public boolean hasNext() {
+            return index < size;
+        }
+
+        @Override
+        public T next() {
+            T result = items[index];
+            index++;
+
+            return result;
+        }
+    }
+
+    public void add(T item) {
+        if (item == null) {
+            throw new IllegalArgumentException();
+        }
+
+        if (size < 100 && !this.contains(item)) {
+            items[size] = item;
+            size++;
+        }
+
+    }
+
+    public boolean contains(T item) {
+        if (item == null) {
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = 0; i < this.size; i++) {
+            if (item == this.items[i]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    // EXERCISE 11.2  EQUALS
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean equals(Object that) {
+        if (this == that) return true;
+        if (that == null) return false;
+        if (!(that instanceof ARSet)) return false;
+        ARSet<T> thatSet = (ARSet<T>) that;
+        if (this.size == thatSet.size) {
+            for (T item : this) {
+                if (!thatSet.contains(item)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ lab11/Dog/Dog.java
@@ -0,0 +1,56 @@
+package lab11.Dog;
+
+public class Dog implements Comparable<Dog> {
+
+    private String name;
+    private int weight;
+
+    public Dog(String n, int w) {
+        name = n;
+        weight = w;
+    }
+
+    /**
+     * @param other another Dog
+     * @return negative number if this is smaller than other
+     *         0 if this equals other
+     *         positive number if this is larger than other
+     */
+    @Override
+    public int compareTo(Dog other) {
+
+//        if (this.weight < other.weight) {
+//            return -1;
+//        } else if (this.weight == other.weight) {
+//            return 0;
+//        }
+//        return 1;
+
+        return this.weight - other.weight;
+
+    }
+
+    public void bark() {
+        System.out.println(name + ": bark!");
+    }
+
+
+    /*
+     ***************************
+     * DO NOT MODIFY CODE ABOVE
+     ***************************
+     */
+
+
+    // LAB EXERCISE 11.2  EQUALS
+
+    @Override
+    public boolean equals(Object other) {
+		if (!(other instanceof Dog)) return false;
+        if (this.weight == ((Dog) other).weight) {
+            return this.name == ((Dog) other).name;
+        }
+        return false;
+    }
+
+}
